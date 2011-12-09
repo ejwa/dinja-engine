3df84de5e75f8da133d916025bb01369276c0c2c
Adam Waldenberg <adam.waldenberg@ejwa.se>
2011-12-09 08:59:55 +0100

    Added model classes to describe a mesh and it's components.

---
+++ engine/src/main/java/com/ejwa/dinja/engine/model/mesh/Mesh.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2011 Ejwa Software. All rights reserved.
+ *
+ * This file is part of Dinja Engine. Dinja Engine is a OpenGLES2
+ * 3D engine with physics support developed for the Android platform.
+ *
+ * Dinja Engine is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Dinja Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with Dinja Engine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package com.ejwa.dinja.engine.model.mesh;
+
+import com.ejwa.dinja.opengles.GLException;
+import com.ejwa.dinja.opengles.primitive.PrimitiveType;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Mesh {
+	private final PrimitiveType primitiveType;
+	private final List<Face> faces = new LinkedList<Face>();
+	private final List<Vertex> vertices = new LinkedList<Vertex>();
+
+	public Mesh(PrimitiveType primitiveType) {
+		this.primitiveType = primitiveType;
+	}
+
+	public Mesh(PrimitiveType type, Vertex ...vertices) {
+		this(type);
+		addVertices(vertices);
+	}
+
+	public Mesh(PrimitiveType primitiveType, Vertex vertices[], Face ...faces) {
+		this(primitiveType, vertices);
+		addFaces(faces);
+
+	}
+
+	public final void addVertices(Vertex ...vertices) {
+		this.vertices.addAll(Arrays.asList(vertices));
+	}
+
+	public final void addFaces(Face ...faces) {
+		for (Face f : faces) {
+			if (!vertices.containsAll(f.getVertices())) {
+				throw new GLException("Invalid faces list passed to mesh.");
+			}
+
+			this.faces.add(f);
+		}
+	}
+
+	public PrimitiveType getType() {
+		return primitiveType;
+	}
+
+	public List<Vertex> getVertices() {
+		return vertices;
+	}
+
+	public List<Face> getFaces() {
+		return faces;
+	}
+}
---
+++ engine/src/main/java/com/ejwa/dinja/engine/model/mesh/Vertex.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright © 2011 Ejwa Software. All rights reserved.
+ *
+ * This file is part of Dinja Engine. Dinja Engine is a OpenGLES2
+ * 3D engine with physics support developed for the Android platform.
+ *
+ * Dinja Engine is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Dinja Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with Dinja Engine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package com.ejwa.dinja.engine.model.mesh;
+
+import javax.vecmath.Color4f;
+import javax.vecmath.Vector2f;
+import javax.vecmath.Vector3f;
+
+public class Vertex {
+	private Color4f color;
+	private Vector3f normal;
+	private Vector3f position;
+	private Vector2f textureCoordinates;
+
+	public Vertex() {
+		/* Currently, no need to do anything here. */
+	}
+
+	public Vertex(Vector3f position, Vector3f normal) {
+		this.position = position;
+		this.normal = normal;
+	}
+
+	public Vertex(Vector3f position, Vector3f normal, Color4f color) {
+		this(position, normal);
+		this.color = color;
+	}
+
+	public Color4f getColor() {
+		return color;
+	}
+
+	public void setColor(Color4f color) {
+		this.color = color;
+	}
+
+	public Vector3f getNormal() {
+		return normal;
+	}
+
+	public void setNormal(Vector3f normal) {
+		this.normal = normal;
+	}
+
+	public Vector3f getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector3f position) {
+		this.position = position;
+	}
+
+	public Vector2f getTextureCoordinates() {
+		return textureCoordinates;
+	}
+
+	public void setTextureCoordinates(Vector2f textureCoordinates) {
+		this.textureCoordinates = textureCoordinates;
+	}
+}
